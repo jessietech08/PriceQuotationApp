d6e3015b85c9acc0ad728ce35ef5e82b1d76b1bd
Jessie <jessietajalle08@gmail.com>
2024-08-08 18:13:07 -0700

    Created PriceQuotationController

---
+++ PriceQuotationApp/Controllers/PriceQuotationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuotationApp.Models;
+
+namespace PriceQuotationApp.Controllers
+{
+    public class PriceQuotationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculation c)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(c);
+            }
+            return View(c);
+        }
+
+        public IActionResult Clear()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
